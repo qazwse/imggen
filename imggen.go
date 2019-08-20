c20b3c70e32c98949bc751dfea2207880e6316d2
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2019-08-20 11:53:26 -0400

    Make file writing and initial generation more generic

--- imggen.go
+++ imggen.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bytes"
-	"encoding/base64"
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
+	"io/ioutil"
+	"math"
 )
 
 // Image is..
@@ -42,27 +42,38 @@ func (i *Image) generate(fn func(int, int) uint8) {
 }
 
 func test1(x, y int) uint8 {
-	return uint8(x + y)
+	return uint8(math.Sqrt(float64(x * y)))
+}
+
+func blue(x, y int) uint8 {
+	return uint8(0)
 }
 
 func makeimage(x, y int) Image {
+	// Generates an image by default
 	m := Image{make([][]uint8, y), x, y, color.RGBAModel}
-	m.generate(test1)
+	m.generate(blue)
 
 	return m
 }
 
-func showimage(m image.Image) {
+func writeimage(m image.Image, fn string) {
 	var buf bytes.Buffer
 	err := png.Encode(&buf, m)
 	if err != nil {
 		panic(err)
 	}
-	enc := base64.StdEncoding.EncodeToString(buf.Bytes())
-	fmt.Println("IMAGE:" + enc)
+
+	err = ioutil.WriteFile(fn, buf.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
 	m := makeimage(255, 255)
-	showimage(m)
+	writeimage(m, "white.png")
+	m.generate(test1)
+	writeimage(m, "img.png")
 }
